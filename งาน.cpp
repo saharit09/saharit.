e504e9eb38263632b4014eaf0c12440bed6f693d
saharit09 <52209310+saharit09@users.noreply.github.com>
2021-01-12 13:18:50 +0700

    Add files via upload

---
+++ งาน.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " 1st Term of 2020       \6206021620167 Saharit  Imjit " << endl ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " | Subject ID | Subject name             | Score (100) | Grad | \n"  ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " |   4570034  | Introduction to computer |      85     |  'A' | \n"  ;
+	cout << " |   4580247  | English                  |      74     |  'B' | \n"  ;
+	cout << " |   4530001  | Design Thinking          |      92     |  'A' | \n"  ;
+	cout << " ----------------------------------------------------------------------- " << endl ;
+	cout << " |   Total    | Subject = 3              | Average = 3.87     | \n"  ; 
+	cout << " -------------------------*----- " ;
+	return 0 ;
+}
+
